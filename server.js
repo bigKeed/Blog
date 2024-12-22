e4c961db0b1a0063d32a8319658d285cb1086c4c
bigKeed <138622264+bigKeed@users.noreply.github.com>
2024-12-22 22:19:05 +0100

    commit

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,'0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 }); 
